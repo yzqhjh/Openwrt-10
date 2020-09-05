d2c4806a522cff7f1f45fb84dd7c6b0b1ab8b82d
yzqhjh <38723310+yzqhjh@users.noreply.github.com>
2020-09-05 21:40:17 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -6,15 +6,15 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' ./package/base-files/files/bin/config_generate
 
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt/g' ./package/base-files/files/bin/config_generate
 
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 'koolshare' ./package/lean/default-settings/files/zzz-default-settings
 
 
 # 使用源码自带ShadowSocksR Plus+出国软件
